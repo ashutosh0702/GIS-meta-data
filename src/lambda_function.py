8e5f5cf1206a2cc6842aa63b942707c0ccf9e7fd
Ashutosh Baruah <ashutosh.baruah2@escortskubota.com>
2023-09-16 21:06:57 +0000

    Removed bbox expansion logic

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -8,43 +8,6 @@ import utm
 s3 = boto3.client("s3")
 bucket_name = "boundary-plot"
 
-def expand_bbox(bbox, margin):
-    min_lon, min_lat, max_lon, max_lat = bbox
-
-    # Expand the bounding box by adding/subtracting the margin
-    min_lon -= margin
-    min_lat -= margin
-    max_lon += margin
-    max_lat += margin
-
-    return [[min_lon, min_lat], [max_lon, max_lat]]
-
-def stretch_bbox(bbox, gsd):
-    
-    stretch_amount = gsd / 2.0
-
-    lat1, lon1 = bbox[0]
-    utm_x1, utm_y1, _, _ = utm.from_latlon(lat1, lon1)
-
-    lat2, lon2 = bbox[1]
-    utm_x2, utm_y2, _, _ = utm.from_latlon(lat2, lon2)
-
-    stretched_utm_bbox = [
-        [utm_x1 - stretch_amount, utm_y1 - stretch_amount],
-        [utm_x2 + stretch_amount, utm_y2 + stretch_amount]
-    ]
-
-    latlon1 = utm.to_latlon(stretched_utm_bbox[0][0], stretched_utm_bbox[0][1], zone_number=utm.from_latlon(lat1, lon1)[2], northern=lat1 >= 0)
-    latlon2 = utm.to_latlon(stretched_utm_bbox[1][0], stretched_utm_bbox[1][1], zone_number=utm.from_latlon(lat2, lon2)[2], northern=lat2 >= 0)
-
-    stretched_bbox = [list(latlon1), list(latlon2)]
-
-    print(stretch_bbox)
-    
-    return stretched_bbox
-
-
-
 
 def get_bbox_from_geojson(bucket_name, key_name):
     
@@ -63,17 +26,8 @@ def get_bbox_from_geojson(bucket_name, key_name):
     
     coordinates = geojson_data['geometry']['coordinates']
     polygon = Polygon(coordinates[0])
-    b = polygon.bounds
-    bbox = expand_bbox(b, 0)
-
-
-    #bbox = [[b[0],b[1]],[b[2],b[3]]]
-    print(bbox, type(bbox))
-    # Stretch the bbox by 10 meters GSD
-    stretched_bbox = stretch_bbox(bbox, gsd=2)
-
-    # The new bbox after stretching in latitude and longitude format
-    print(stretched_bbox)
+    bbox = polygon.bounds
+   
     
     polygon = Polygon(coordinates[0])
     wkt = polygon.wkt
